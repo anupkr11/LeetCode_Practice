1dc3f9da49f26651c5dbc3f399842090413ca3dc
Anup Kumar <99500785+anupkr11@users.noreply.github.com>
2026-02-26 11:54:39 +0530

    Time: 1 ms (72.22%) | Memory: 43.4 MB (60.85%) - LeetSync

---
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minNumber(int[] nums1, int[] nums2) {
+        int val = Integer.MAX_VALUE;
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
+                if(nums1[i] == nums2[j])  val = Math.min(val,nums1[i]);
+                val = Math.min(val,Math.min(nums1[i]*10+nums2[j],nums2[j]*10+nums1[i]));
+            }
+        }
+        return val;
+    }
+}
